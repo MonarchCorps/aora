20647cec4d3a2c9569fb742c8f7df338f53fd616
Monarch Corps <monarchcorps01@gmail.com>
2025-02-23 17:25:43 +0100

    checked if a user is authenticated then redirect to the home page

--- app/index.jsx
+++ app/index.jsx
@@ -3,9 +3,16 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '@/constants'
 import CustomButton from '@/components/CustomButton'
 import { StatusBar } from 'expo-status-bar'
-import { router } from 'expo-router'
+import { Redirect, router } from 'expo-router'
+import useAuth from '@/hooks/useAuth'
+import { navigate } from '@/helper/navigate'
 
 export default function Index() {
+	const { auth, loading } = useAuth()
+
+	if (loading) return null
+	if (auth?._id) return <Redirect href='/home' />
+
 	return (
 		<SafeAreaView className='bg-primary h-full'>
 			<ScrollView contentContainerStyle={{ height: '100%' }}>
@@ -16,12 +23,12 @@ export default function Index() {
 						<Text className='text-3xl text-[#fff] font-bold text-center'>
 							Discover Endless Possibilities with {' '}
 							<View className='relative'>
-								<Text className='text-u-secondary-200 font-bold text-3xl mt-[2.7rem]'>Aora</Text>
+								<Text className='text-secondary-200 font-bold text-3xl mt-[2.7rem]'>Aora</Text>
 								<Image source={images.path} className='w-[120px] h-[15px] absolute -bottom-3 -right-10' resizeMode='contain' />
 							</View>
 						</Text>
 						<Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
-						<CustomButton title='Continue with Email' containerStyles='mt-7' handlePress={() => router.push('/sign-in')} />
+						<CustomButton title='Continue with Email' containerStyles='mt-7' handlePress={() => navigate('/sign-in')} />
 					</View>
 				</View>
 			</ScrollView>
