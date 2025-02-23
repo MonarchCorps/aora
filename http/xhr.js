8c898584dd0add1dd9d8ccf5617eb2ad7825082f
Monarch Corps <monarchcorps01@gmail.com>
2025-02-23 17:18:15 +0100

    fix: not redirecting to sign in page on a 401 response

--- http/xhr.js
+++ http/xhr.js
@@ -1,5 +1,5 @@
 import { CONFIGS } from "../configs/index";
-import { getAccessToken, storeAccessToken, removeAccessToken, removeRefreshToken } from "../helper/tokens";
+import { getAccessToken, storeAccessToken, getRefreshToken, clearStorage } from "../helper/tokens";
 import { replace } from "../helper/navigate";
 import axios from "axios";
 
@@ -14,51 +14,65 @@ export const axiosPrivate = axios.create({
 });
 
 const $http = axiosPrivate;
-let interceptorsSet = false;
 
-const setupInterceptors = (setAuth, refresh) => {
-    if (interceptorsSet) return;
+$http.interceptors.request.use(
+    async (config) => {
+        let token = await getAccessToken();
 
-    $http.interceptors.request.use(
-        async (config) => {
-            let token = await getAccessToken();
+        if (token && !config.headers["Authorization"]) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
-            if (token && !config.headers["Authorization"]) {
-                config.headers["Authorization"] = `Bearer ${token}`;
-            }
-            return config;
-        },
-        (error) => Promise.reject(error)
-    );
+$http.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const prevRequest = error?.config;
+        if (error?.response?.status === 403 && !prevRequest?.sent) {
+            prevRequest.sent = true;
+            try {
+                const newAccessToken = await refreshAuthLogic();
+                // if (!newAccessToken) console.error("Failed to refresh token");
 
-    $http.interceptors.response.use(
-        (response) => response,
-        async (error) => {
-            const prevRequest = error?.config;
-
-            if (error?.response?.status === 403 && !prevRequest?.sent) {
-                prevRequest.sent = true;
-                try {
-                    const newAccessToken = await refresh();
-                    await storeAccessToken(newAccessToken);
-
-                    prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
-                    return $http(prevRequest);
-                } catch (refreshError) {
-                    console.error("Refresh token expired:", refreshError);
-                    if (refreshError.response?.status === 401) {
-                        setAuth({});
-                        await removeAccessToken();
-                        await removeRefreshToken();
-                        replace('/sign-in');
-                    }
+                await storeAccessToken(newAccessToken);
+                prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+                return $http(prevRequest);
+            } catch (refreshError) {
+                if (refreshError.response?.status === 401) {
+                    replace('/sign-in');
+                    await clearStorage()
                 }
             }
-            return Promise.reject(error);
+        } else if (error?.response?.status === 401) {
+            replace('/sign-in');
+            await clearStorage()
         }
-    );
 
-    interceptorsSet = true;
+        return Promise.reject(error);
+    }
+);
+
+
+const refreshAuthLogic = async () => {
+    try {
+        const response = await axios.post(
+            `${baseURL}/auth/${CONFIGS.URL.API_VERSION}/refresh`,
+            {
+                refreshToken: await getRefreshToken()
+            },
+            {
+                withCredentials: true
+            });
+
+        return response.data;
+    } catch (error) {
+        if (error.response?.status === 401) {
+            console.error("Refresh token expired or invalid");
+        }
+    }
 }
 
-export { $http, setupInterceptors };
+export { $http };
