562f790ab81ed37e7db8d81193739d26b6db0541
Monarch Corps <monarchcorps01@gmail.com>
2025-02-23 17:31:11 +0100

    search query page

---
+++ app/search/[query].jsx
@@ -0,0 +1,70 @@
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '@/constants'
+import SearchInput from '@/components/SearchInput'
+import Trending from '@/components/Trending'
+import EmptyState from '@/components/EmptyState'
+import { useEffect, useState } from 'react'
+import VideoCard from '@/components/VideoCard'
+import { items, trendingPosts } from '@/db'
+import { useLocalSearchParams } from 'expo-router'
+
+const Search = () => {
+    const { query } = useLocalSearchParams()
+    // const [refreshing, setRefreshing] = useState(false)
+
+    // const onRefresh = async () => {
+    //     setRefreshing(true)
+    //     setRefreshing(false)
+    // }
+
+    // useEffect(() => {
+    //     refetch()
+    // }, [query])
+
+    return (
+        <SafeAreaView className='bg-primary h-full'>
+            <FlatList
+                data={items}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <VideoCard video={item} />
+                    )
+                }}
+                ListHeaderComponent={() => (
+                    <View className='my-6 px-4'>
+                        <Text className='font-pmedium text-sm text-gray-100'>
+                            Search Results
+                        </Text>
+                        <Text className='text-2xl font-psemibold text-white'>
+                            {query}
+                        </Text>
+
+                        <View className='mt-6 mb-6'>
+                            <SearchInput initialQuery={query} />
+                        </View>
+
+                        <View className='w-full flex-1 pt-5 pb-8'>
+                            <Text className='text-gray-100 text-lg font-pregular mb-3'>
+                                Trending Videos
+                            </Text>
+                            <Trending posts={trendingPosts} />
+                        </View>
+
+                    </View>
+                )}
+                ListEmptyComponent={() => {
+                    return (
+                        <EmptyState
+                            title='No videos found'
+                            subTitle='No videos found for this search query'
+                        />
+                    )
+                }}
+            />
+        </SafeAreaView>
+    )
+}
+
+export default Search
